8b03fa7b57a9449938e6e3e02528f7ae65974001
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-10-18 13:25:46 +0530

    Initial Commit

---
+++ CineBrowse/CineBrowseApp.swift
@@ -0,0 +1,17 @@
+//
+//  CineBrowseApp.swift
+//  CineBrowse
+//
+//  Created by Sajan Lawrence on 18/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CineBrowseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CineBrowse/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CineBrowse
+//
+//  Created by Sajan Lawrence on 18/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
