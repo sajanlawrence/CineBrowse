637b1ea5617938b9405f44a7076ef3cf54142b56
Sajan Lawrence <sajanlawrence12345@gmail.com>
2025-10-19 22:20:13 +0530

    Improved code readability

--- CineBrowse/Views/CastRowView.swift
+++ CineBrowse/Views/CastRowView.swift
@@ -12,35 +12,49 @@ struct CastRowView: View {
     @Environment(\.colorScheme) var colorScheme
     var body: some View {
         VStack(alignment: .center){
-            AsyncImage(url: URL(string: cast.profilePath)) { image in
-                image
-                    .resizable()
-                    .scaledToFill()
-            } placeholder: {
-                Color.gray.opacity(0.3)
-                    .overlay {
-                        Image(systemName: "person.crop.circle.fill")
-                            .resizable()
-                            .scaledToFit()
-                            .padding(16)
-                            .foregroundStyle(.gray)
-                    }
-            }
-            .frame(width: 80, height: 100)
-            .clipShape(RoundedRectangle(cornerRadius: 8))
-            .shadow(color: colorScheme == .dark ? .white : .black, radius: 3)
-            Text(cast.name)
-                .font(.caption)
-                .fontWeight(.semibold)
-                .lineLimit(1)
-                .frame(width: 80)
-            Text(cast.character)
-                .font(.caption2)
-                .foregroundStyle(.secondary)
-                .lineLimit(2)
-                .frame(width: 100)
-                .multilineTextAlignment(.center)
+            imageView
+            nameView
+            characterView
         }
+    }
+}
+
+extension CastRowView{
+    private var imageView: some View{
+        AsyncImage(url: URL(string: cast.profilePath)) { image in
+            image
+                .resizable()
+                .scaledToFill()
+        } placeholder: {
+            Color.gray.opacity(0.3)
+                .overlay {
+                    Image(systemName: "person.crop.circle.fill")
+                        .resizable()
+                        .scaledToFit()
+                        .padding(16)
+                        .foregroundStyle(.gray)
+                }
+        }
+        .frame(width: 80, height: 100)
+        .clipShape(RoundedRectangle(cornerRadius: 8))
+        .shadow(color: colorScheme == .dark ? .white : .black, radius: 3)
+    }
+    
+    private var nameView: some View{
+        Text(cast.name)
+            .font(.caption)
+            .fontWeight(.semibold)
+            .lineLimit(1)
+            .frame(width: 80)
+    }
+    
+    private var characterView: some View{
+        Text(cast.character)
+            .font(.caption2)
+            .foregroundStyle(.secondary)
+            .lineLimit(2)
+            .frame(width: 100)
+            .multilineTextAlignment(.center)
     }
 }
 
--- CineBrowse/Views/MovieDetailView.swift
+++ CineBrowse/Views/MovieDetailView.swift
@@ -14,71 +14,86 @@ struct MovieDetailView: View {
     var body: some View {
         VStack{
             ScrollView{
-                KFImage(URL(string: movie.posterPath))
-                    .placeholder {
-                        Color.gray.opacity(0.3)
-                            .overlay {
-                                ProgressView()
-                            }
-                    }
-                    .cancelOnDisappear(true)
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(15)
-                    .frame(width: 300, height: 250)
-                    .clipped()
-                    .padding()
-                    .shadow(color: colorScheme == .dark ? .white : .black, radius: 15)
-                Text(movie.originalTitle)
-                    .font(.title2)
-                    .bold()
-                    .lineLimit(2)
-                    .frame(maxWidth: .infinity)
-                    .multilineTextAlignment(.center)
-                    .padding(10)
-                
-                Text(movie.overview)
-                    .multilineTextAlignment(.center)
-                    .padding(.horizontal, 10)
-                    .font(.subheadline)
-                    .foregroundStyle(.secondary)
-                
+                imageView
+                titleView
+                overviewView
                 Divider()
-                
-                HStack(spacing: 100) {
-                    VStack(alignment: .leading, spacing: 12) {
-                        InfoRow(label: "Original Language", value: movie.originalLanguage.uppercased(), systemImage: "globe")
-                        InfoRow(label: "Popularity", value: String(format: "%.2f", movie.popularity), systemImage: "star.fill")
-                        InfoRow(label: "Release Date", value: movie.releaseDate?.formattedMovieDate ?? "NA", systemImage: "calendar")
-                    }
-                    .padding(.vertical)
-                    
-                    VStack(alignment: .leading, spacing: 12) {
-                        InfoRow(label: "Vote Average", value: String(format: "%.2f", movie.voteAverage), systemImage: "hand.thumbsup.fill")
-                        InfoRow(label: "Vote Count", value: "\(movie.voteCount)", systemImage: "number")
-                       Spacer()
-                    }
-                    .padding(.vertical)
-                }
-                
+                infoView
                 Divider()
-                HStack(alignment: .center) {
-                    Text("Cast")
-                        .padding(.leading)
-                        .padding(.top, 2)
-                        .font(.title2)
-                    Spacer()
-                }
+                castView
                 CastListView(casts: movie.casts)
                     .frame(height: 170)
                     .padding(.leading)
-                    
-                    
             }
         }
     }
 }
 
+extension MovieDetailView{
+    private var imageView: some View{
+        KFImage(URL(string: movie.posterPath))
+            .placeholder {
+                Color.gray.opacity(0.3)
+                    .overlay {
+                        ProgressView()
+                    }
+            }
+            .cancelOnDisappear(true)
+            .resizable()
+            .scaledToFit()
+            .cornerRadius(15)
+            .frame(width: 300, height: 250)
+            .clipped()
+            .padding()
+            .shadow(color: colorScheme == .dark ? .white : .black, radius: 15)
+    }
+    
+    private var titleView: some View{
+        Text(movie.originalTitle)
+            .font(.title2)
+            .bold()
+            .lineLimit(2)
+            .frame(maxWidth: .infinity)
+            .multilineTextAlignment(.center)
+            .padding(10)
+    }
+    
+    private var overviewView: some View{
+        Text(movie.overview)
+            .multilineTextAlignment(.center)
+            .padding(.horizontal, 10)
+            .font(.subheadline)
+            .foregroundStyle(.secondary)
+    }
+    
+    private var infoView: some View{
+        HStack(spacing: 100) {
+            VStack(alignment: .leading, spacing: 12) {
+                InfoRow(label: "Original Language", value: movie.originalLanguage.uppercased(), systemImage: "globe")
+                InfoRow(label: "Popularity", value: String(format: "%.2f", movie.popularity), systemImage: "star.fill")
+                InfoRow(label: "Release Date", value: movie.releaseDate?.formattedMovieDate ?? "NA", systemImage: "calendar")
+            }
+            .padding(.vertical)
+            
+            VStack(alignment: .leading, spacing: 12) {
+                InfoRow(label: "Vote Average", value: String(format: "%.2f", movie.voteAverage), systemImage: "hand.thumbsup.fill")
+                InfoRow(label: "Vote Count", value: "\(movie.voteCount)", systemImage: "number")
+               Spacer()
+            }
+            .padding(.vertical)
+        }
+    }
+    
+    private var castView: some View {
+        HStack(alignment: .center) {
+            Text("Cast")
+                .padding(.leading)
+                .padding(.top, 2)
+                .font(.title2)
+            Spacer()
+        }
+    }
+}
 
 #Preview {
     MovieDetailView(movie: .default)
